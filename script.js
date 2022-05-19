c9b7870d8e016b65f5c1ca140752f08a15030f48
Davron <davron1104@yandex.com>
2022-05-19 23:47:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let storeName = 'makro';
+const storeDescription = {
+    budget: 10000,
+    employees: ['davron', 'david', 'dunkey'],
+    products: {
+        socks: 100,
+        jacket: 200
+    },
+    open: true
+};
